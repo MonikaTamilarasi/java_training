794a09293b10a90e5a2f0911f71e393b066b6d5d
Monikatamilarasi.N <Monikatamilarasi.N@EXIND3010-23.allfon.in>
2022-04-20 18:06:34 +0530

    practical ques 3

---
+++ practical_ques3/src/com/training/Application.java
@@ -0,0 +1,28 @@
+package com.training;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.training.model.Sorting;
+
+public class Application {
+	public static void main(String[] args) {
+		ArrayList <String> fruits=new ArrayList<>();
+		fruits.add("apple");
+		fruits.add("orange");
+		fruits.add("apple");
+		fruits.add("pineapple");
+		fruits.add("orange");
+		fruits.add("banana");
+		
+		Collections.sort(fruits);
+		Set<String> set =new TreeSet<>();
+		set.addAll(fruits);
+		System.out.println(set);
+		
+	}
+
+	}
+
---
+++ practical_ques3/src/com/training/model/Sorting.java
@@ -0,0 +1,31 @@
+package com.training.model;
+
+public class Sorting {
+	private String fruitName;
+
+	public Sorting() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sorting(String fruitName) {
+		super();
+		this.fruitName = fruitName;
+	}
+
+	public String getFruitName() {
+		return fruitName;
+	}
+
+	public void setFruitName(String fruitName) {
+		this.fruitName = fruitName;
+	}
+
+	@Override
+	public String toString() {
+		return "Sorting [fruitName=" + fruitName + "]";
+	}
+	
+	
+	
+}
