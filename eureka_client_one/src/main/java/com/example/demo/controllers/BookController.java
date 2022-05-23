41592f603139d8c0281c46c215c5d4740501e743
Monikatamilarasi.N <Monikatamilarasi.N@EXIND3010-23.allfon.in>
2022-05-23 10:56:30 +0530

    client one eureka

---
+++ eureka_client_one/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/v1")
+
+public class BookController {
+	
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping(path = "/books")
+	public List<String> getBooks(){
+		return Arrays.asList("Head First Java","Spring In Action",
+				"Thinking In Java");
+	}
+	
+	@GetMapping(path = "/books/{id}")
+	public String getBookById(@PathVariable int id) {
+		if(id==1) {
+			return "Head First Java :="+port;
+		}else {
+			return "Spring In Action :="+port;
+		}
+	}
+}
